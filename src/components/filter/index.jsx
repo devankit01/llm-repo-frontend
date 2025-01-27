f19c6175ba92f7d01cc42d8c021c5ead3f201860
NIKHIL KUMAR GUPTA <94856084+Nikhil-1402@users.noreply.github.com>
2025-01-28 02:14:20 +0530

    dynamic tags in filter fetched from data

--- src/components/filter/index.jsx
+++ src/components/filter/index.jsx
@@ -1,13 +1,17 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FaSearch, FaChevronDown, FaChevronUp } from "react-icons/fa";
 import { useDispatch, useSelector } from "react-redux";
 import { setSearchText, setTag } from "../../redux/slice/filterSlice";
+import { useLocation } from "react-router-dom";
 
 const Filter = () => {
+  const location = useLocation().pathname;
   const [enteredSearchText, setEnteredSearchText] = useState("");
   const [showAllTags, setShowAllTags] = useState(false);
   const [isTagsVisible, setIsTagsVisible] = useState(true);
+  const [tags, setTags] = useState([]);
   const tagList = useSelector((state) => state.filter.tags);
+  const { llmtool, gpt, llmlibrary } = useSelector((state) => state.filter);
   const dispatch = useDispatch();
 
   const handleSearchChange = (e) => {
@@ -30,35 +34,27 @@ const Filter = () => {
     dispatch(setTag(tagName.toLowerCase()));
   };
 
-  const tags = [
-    { id: 1, name: "speech synthesis" },
-    { id: 2, name: "text-to-speech" },
-    { id: 3, name: "voice generation" },
-    { id: 4, name: "deep learning" },
-    { id: 5, name: "natural language processing" },
-    { id: 6, name: "text-based" },
-    { id: 7, name: "language model" },
-    { id: 8, name: "finance" },
-    { id: 9, name: "open source" },
-    { id: 10, name: "deep contexualized embeddings" },
-    { id: 11, name: "transformer" },
-    { id: 12, name: "efficient model" },
-    { id: 13, name: "dialogue systems" },
-    { id: 14, name: "text generation" },
-    { id: 15, name: "conversational ai" },
-    { id: 16, name: "pretraining" },
-    { id: 17, name: "string" },
-    { id: 18, name: "llm" },
-    { id: 19, name: "llm training" },
-    { id: 20, name: "llm deployment" },
-    { id: 21, name: "rag" },
-    { id: 22, name: "sql" },
-    { id: 23, name: "llm finetune" },
-    { id: 24, name: "document parsing" },
-    { id: 25, name: "assistant" },
-    { id: 26, name: "programming" },
-    { id: 27, name: "writing" },
-  ];
+  const extractUniqueTags = (data, separator) => {
+    const allTags = data.flatMap((item) => {
+      const tags = location === "/gpt" ? item.category : item.tags;
+      if (!tags || tags.trim() === "") return;
+      return tags
+        .split(separator)
+        .map((tag) => tag.trim())
+        .filter((tag) => tag);
+    });
+    return [...new Set(allTags)];
+  };
+
+  useEffect(() => {
+    location === "/llm-tools"
+      ? setTags(extractUniqueTags(llmtool, ","))
+      : location === "/llm-library"
+      ? setTags(extractUniqueTags(llmlibrary, ";"))
+      : setTags(extractUniqueTags(gpt, ";"));
+  }, [location, llmtool, llmlibrary, gpt]);
+
+  console.log(tagList);
 
   return (
     <section className="bg-[#202330] py-8 px-5 lg:h-[88.5vh] lg:overflow-y-scroll w-full lg:rounded-none rounded-xl">
@@ -108,20 +104,23 @@ const Filter = () => {
           {isTagsVisible && (
             <div>
               <ul className="flex flex-wrap gap-x-2 gap-y-2">
-                {(showAllTags ? tags : tags.slice(0, 10)).map((tag) => (
-                  <li
-                    key={tag.id}
-                    onClick={() => handleTagClick(tag.name)}
-                    className={`border border-gray-600 px-3 py-1 rounded-md text-xs cursor-pointer ease-in-out duration-200 transition-all
-                      ${
-                        tagList.includes(tag.name)
-                          ? "bg-[#7F89FF]/5 text-[#7F89FF] border-[#7F89FF]"
-                          : "hover:text-[#7F89FF] hover:bg-[#7F89FF]/5 hover:border-[#7F89FF]"
-                      }`}
-                  >
-                    {tag?.name?.toUpperCase()}
-                  </li>
-                ))}
+                {(showAllTags ? tags : tags.slice(0, 10)).map(
+                  (tag, i) =>
+                    tag !== undefined && (
+                      <li
+                        key={i}
+                        onClick={() => handleTagClick(tag)}
+                        className={`border border-gray-600 px-3 py-1 rounded-md text-xs cursor-pointer ease-in-out duration-200 transition-all
+                    ${
+                      tagList.includes(tag.toLowerCase())
+                        ? "bg-[#7F89FF]/5 text-[#7F89FF] border-[#7F89FF]"
+                        : "hover:text-[#7F89FF] hover:bg-[#7F89FF]/5 hover:border-[#7F89FF]"
+                    }`}
+                      >
+                        {tag?.toUpperCase()}
+                      </li>
+                    )
+                )}
               </ul>
 
               <button
